015b6fb09a16143d79d3c7c871b8c20e18ae15c7
gboehl <admin@gregorboehl.com>
2019-10-14 19:51:04 +0200

    add experimental refiltering

--- econsieve/ipas.py
+++ econsieve/ipas.py
@@ -8,30 +8,44 @@ from grgrlib.stuff import GPP, timeprint
 from .stats import logpdf
 
 
-def ipas(self, X=None, get_eps=None, means=None, covs=None, ngen=100, npop=10, maxeval=0, ftol=None, method_loc=None, method_glob=None, bound_sigma=4, seed=0, verbose=True):
-    """Iterative Path-Adjusing Smoother. Assumes that either, X (a time series of ensembles) is given (or can be taken from the `self` filter object), or the time series means and covs are give. From the filter object, also `eps_cov` (the diagonal matrix of the standard deviations of the shocks) and the transition function `fx(state, shock_innovations)` must be provided.
+def ipas(self, X=None, get_eps=None, refilter=False, means=None, covs=None, ngen=100, npop=10, maxeval=0, ftol=None, method_loc=None, method_glob=None, bound_sigma=4, seed=0, verbose=True):
+    """Iterative Path-Adjusing Smoother. Assumes that either, X (a time series of ensembles) is given (or can be taken from the `self` filter object), or the time series means and covs are give. From the filter object, also `eps_cov` (the diagonal matrix of the standard deviations of the shocks) and the transition function `t_func(state, shock_innovations)` must be provided.
     """
 
     if verbose:
         st = time.time()
 
-    if X is None and hasattr(self, 'Ss'):
-        X = np.rollaxis(self.Ss, 2)
+    if refilter:
+        ## X must be an ensembles of x dimensions
+        if X is None:
+            X = self.Ss[0]
 
-    if means is None:
-        means = np.mean(X, axis=0)
+        means = np.empty((len(self.Z), X.shape[0]))
+        covs = np.empty((len(self.Z), X.shape[0], X.shape[0]))
+        Xs = np.empty((len(self.Z), X.shape[0], self.N))
 
-    res = np.empty((means.shape[0]-1,self._dim_z))
+        means[0] = np.mean(X, axis=1)
+        covs[0] = np.cov(X)
+        Xs[0] = X
 
-    ## presmoothing
-    if get_eps is not None:
-        for i in range(res.shape[0]):
-            res[i,:] = get_eps(means[i], means[i+1])
+        I1 = np.ones(self.N)
+        I2 = np.eye(self.N) - np.outer(I1, I1)/self.N
 
-    if covs is None:
-        covs = np.empty((X.shape[1], X.shape[2], X.shape[2]))
-        for i in range(X.shape[1]):
-            covs[i, :, :] = np.cov(X[:, i, :].T)
+        Y = np.empty((self._dim_z, self.N))
+    else:
+        ## X must be time series of ensembles of x dimensions
+        if X is None and hasattr(self, 'Ss'):
+            X = np.rollaxis(self.Ss, 2)
+
+        if means is None:
+            means = np.mean(X, axis=0)
+
+        if covs is None:
+            covs = np.empty((X.shape[1], X.shape[2], X.shape[2]))
+            for i in range(X.shape[1]):
+                covs[i, :, :] = np.cov(X[:, i, :].T)
+
+    x = means[0]
 
     if method_loc is None:
         method_loc = 'cobyla'
@@ -43,15 +57,12 @@ def ipas(self, X=None, get_eps=None, means=None, covs=None, ngen=100, npop=10, m
 
     def maintarget(eps, x, mean, cov):
 
-        state, flag = self.fx(x, eps)
+        state, flag = self.t_func(x, eps)
 
         if flag:
             return -np.inf
 
         return logpdf(state, mean=mean, cov=cov)
-
-    x = means[0]
-    flag = False
 
     if ngen:
         algo_glob = pg.algorithm(method_glob(gen=ngen, seed=seed))
@@ -70,17 +81,46 @@ def ipas(self, X=None, get_eps=None, means=None, covs=None, ngen=100, npop=10, m
     else:
         def wrap(x): return x
 
+    ## preallocate
+    res = np.empty((len(self.Z)-1,self._dim_z))
+    flag = False
+
     for t in wrap(range(means.shape[0] - 1)):
 
+        if refilter:
+            # predict
+            for i in range(X.shape[1]):
+                eps = np.random.multivariate_normal(mean=np.zeros(self._dim_z), cov=self.Q)
+                X[:, i] = self.t_func(X[:, i], eps)[0]
+
+            Y = self.o_func(X.T).T
+
+            mus = np.random.multivariate_normal(mean=np.zeros(self._dim_z), cov=self.R, size=self.N)
+
+            # # update
+            X_bar = X @ I2
+            Y_bar = Y @ I2
+            ZZ = np.outer(self.Z[t+1], I1)
+            S = np.cov(Y) + self.R
+            X += X_bar @ Y_bar.T @ np.linalg.inv((self.N-1)*S) @ (ZZ - Y - mus.T)
+
+            mean = np.mean(X, axis=1)
+            cov = np.cov(X)
+        else:
+            mean = means[t+1]
+            cov = covs[t+1]
+
+        if get_eps is not None:
+            guess = get_eps(x, mean)
+
         def func(eps): 
-            return maintarget(eps, x, means[t+1], covs[t+1])
+            return maintarget(eps, x, mean, cov)
 
         prob = pg.problem(GPP(func=func, bounds=(-bound, bound)))
-
         pop = pg.population(prob, npop-1-(get_eps is not None), seed=seed)
 
         if get_eps is not None:
-            pop.push_back(res[t])
+            pop.push_back(guess)
 
         pop.push_back(np.zeros(self._dim_z))
 
@@ -90,14 +130,19 @@ def ipas(self, X=None, get_eps=None, means=None, covs=None, ngen=100, npop=10, m
             pop = algo_loc.evolve(pop)
 
         eps = pop.champion_x
-        x, fflag = self.fx(x, noise=eps)
+        x, fflag = self.t_func(x, noise=eps)
 
         if fflag:
             # should never happen
             flag = True
 
-        res[t,:] = eps
+        res[t] = eps
         means[t+1] = x
+
+        if refilter:
+            X = (X.T + x - np.mean(X, axis=1)).T
+            Xs[t+1] = X
+            covs[t+1] = cov
 
     if flag and verbose:
         print('[ipas:]'.ljust(15, ' ')+'Transition function returned error.')
@@ -105,5 +150,8 @@ def ipas(self, X=None, get_eps=None, means=None, covs=None, ngen=100, npop=10, m
     if verbose:
         print('[ipas:]'.ljust(15, ' ')+'Extraction took ',
               timeprint(time.time() - st, 3))
+
+    if refilter:
+        self.EXs = Xs
 
     return means, covs, res, flag
--- econsieve/tenkf.py
+++ econsieve/tenkf.py
@@ -12,14 +12,15 @@ class TEnKF(object):
 
     name = 'TEnKF'
 
-    def __init__(self, N, dim_x=None, dim_z=None, fx=None, hx=None):
+    def __init__(self, N, dim_x=None, dim_z=None, fx=None, hx=None, seed=None):
 
         self._dim_x = dim_x
         self._dim_z = dim_z
-        self.fx = fx
-        self.hx = hx
+        self.t_func = fx
+        self.o_func = hx
 
         self.N = N
+        self.seed = seed
 
         self.R = np.eye(self._dim_z)
         self.Q = np.eye(self._dim_x)
@@ -27,12 +28,12 @@ class TEnKF(object):
 
         self.x = np.zeros(self._dim_x)
 
-    def batch_filter(self, Z, store=False, calc_ll=False, verbose=False):
+    def batch_filter(self, Z, init_states=None, seed=None, store=False, calc_ll=False, verbose=False):
 
         # store time series for later
         self.Z = Z
 
-        _dim_x, _dim_z, N, P, R, Q, x = self._dim_x, self._dim_z, self.N, self.P, self.R, self.Q, self.x
+        _dim_x, _dim_z, N, P, R, Q = self._dim_x, self._dim_z, self.N, self.P, self.R, self.Q
 
         I1 = np.ones(N)
         I2 = np.eye(N) - np.outer(I1, I1)/N
@@ -45,17 +46,26 @@ class TEnKF(object):
 
         ll = 0
 
+        if seed is not None: 
+            self.seed = seed
+
+        if self.seed is not None:
+            np.random.seed(self.seed)
+
         means = np.empty((Z.shape[0], _dim_x))
         covs = np.empty((Z.shape[0], _dim_x, _dim_x))
 
         Y = np.empty((_dim_z, N))
-        X_prior = np.empty((_dim_x, N))
 
         mus = np.random.multivariate_normal(
             mean=np.zeros(self._dim_z), cov=self.R, size=(len(Z), self.N))
         epss = np.random.multivariate_normal(
-            mean=np.zeros(self._dim_x), cov=self.Q, size=(len(Z), self.N))
-        X = np.random.multivariate_normal(mean=x, cov=P, size=N).T
+            mean=np.zeros(self._dim_z), cov=self.Q, size=(len(Z), self.N))
+
+        if init_states is None:
+            X = np.random.multivariate_normal(mean=self.x, cov=P, size=N).T
+        else:
+            X = init_states
 
         self.Xs = np.empty((Z.shape[0], _dim_x, N))
 
@@ -64,24 +74,23 @@ class TEnKF(object):
             # predict
             for i in range(X.shape[1]):
                 eps = epss[nz, i]
-                X_prior[:, i] = self.fx(X[:, i]+eps)[0]
+                X[:, i] = self.t_func(X[:, i], eps)[0]
 
-            for i in range(X_prior.shape[1]):
-                Y[:, i] = self.hx(X_prior[:, i])
+            Y = self.o_func(X.T).T
+
+            if store:
+                self.X_priors[nz, :, :] = X
 
             # update
-            X_bar = X_prior @ I2
+            X_bar = X @ I2
             Y_bar = Y @ I2
             ZZ = np.outer(z, I1)
             S = np.cov(Y) + R
-            X = X_prior + \
-                X_bar @ Y_bar.T @ nl.inv((N-1)*S) @ (ZZ - Y - mus[nz].T)
-
+            X += X_bar @ Y_bar.T @ nl.inv((N-1)*S) @ (ZZ - Y - mus[nz].T)
 
             if store:
                 self.X_bar_priors[nz, :, :] = X_bar
                 self.X_bars[nz, :, :] = X @ I2
-                self.X_priors[nz, :, :] = X_prior
                 self.Xs[nz, :, :] = X
 
             if calc_ll:
