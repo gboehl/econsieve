c5f3e26fd629b4e6790bd2ab4401dd7b669096bb
Gregor Boehl <admin@gregorboehl.com>
2020-02-13 12:29:12 +0100

    move stuff to grgrlib

--- econsieve/stats.py
+++ econsieve/stats.py
@@ -32,10 +32,8 @@ import scipy.sparse.linalg as spln
 from scipy.stats import norm, multivariate_normal
 
 from numba import njit
+from grgrlib.stats import logpdf
 
-
-# _cond    = 1e6 * np.finfo('d').eps
-_cond    = 1e-9
 
 def _validate_vector(u, dtype=None):
     # this is taken from scipy.spatial.distance. Internal function, so
@@ -114,38 +112,6 @@ def likelihood(z, x, P, H, R):
     covariance error R
     """
     return np.exp(log_likelihood(z, x, P, H, R))
-
-
-@njit(cache=True)
-def psd_func(M):
-
-
-    # Compute the symmetric eigendecomposition.
-    # Note that eigh takes care of array conversion, chkfinite,
-    # and assertion that the matrix is square.
-
-    s, u = np.linalg.eigh(M)
-    eps     = _cond * np.max(np.abs(s))
-    d       = s[s > eps]
-
-    s_pinv  = [0 if abs(x) <= eps else 1/x for x in s]
-    s_pinv_np   = np.array(s_pinv)
-    U       = np.multiply(u, np.sqrt(s_pinv_np))
-
-    return U, np.sum(np.log(d)), len(d)
-
-@njit(cache=True)
-def logpdf(x, mean, cov):
-
-    _LOG_2PI = np.log(2 * np.pi)
-
-    dim     = mean.shape[0]
-    prec_U, log_det_cov, rank   = psd_func(cov)
-    dev     = x - mean
-    maha    = np.sum(np.square(np.dot(dev, prec_U)), axis=-1)
-    out     = -0.5 * (rank * _LOG_2PI + log_det_cov + maha)
-
-    return out
 
 
 def gaussian(x, mean, var, normed=True):
